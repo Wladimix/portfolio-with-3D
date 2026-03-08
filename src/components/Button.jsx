9fdac7021314cdeb947688bc3f3d7fe1da2461e4
Vladimir <m.a.x.i.m.o.v@yandex.ru>
2026-03-08 15:35:00 +0300

    отмена коммита "заметка о плавной прокрутке"
    This reverts commit 906b853d4cc9ca8bdd634457dedaa51cb0c9b531.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = ({ text, className }) => {
 			className={`${className ?? ""} cta-wrapper`}
 			onClick={e => {
 				e.preventDefault();
-				// TODO: сюда добавится прокрутка к следующей секции (на 1:28:45 показана плавная прокрутка)
+				// TODO: сюда добавится прокрутка к следующей секции
 				// const target = document.getElementById("");
 
 				// if (target && id) {
