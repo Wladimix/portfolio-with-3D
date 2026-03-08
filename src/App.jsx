9ae472650d69be398bb3cbcf6279efdd23f88740
Vladimir <m.a.x.i.m.o.v@yandex.ru>
2026-03-08 16:49:13 +0300

    навигационное меню

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import NavBar from "./components/NavBar";
 import Hero from "./sections/Hero";
 
 const App = () => {
 	return (
 		<>
+			<NavBar />
 			<Hero />
 		</>
 	);
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,18 @@
+export const navLinks = [
+	{
+		name: "Опыт",
+		link: "#",
+	},
+	{
+		name: "Навыки",
+		link: "#",
+	},
+	{
+		name: "Обо мне",
+		link: "#",
+	},
+];
+
 export const words = [
 	{ text: "Backend", imgPath: "images/backend.svg" },
 	{ text: "Frontend", imgPath: "images/frontend.svg" },
