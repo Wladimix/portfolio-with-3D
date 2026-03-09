5980702753e4f7a10bfc7b6a4e397484cb88fb6a
Vladimir <m.a.x.i.m.o.v@yandex.ru>
2026-03-09 17:05:36 +0300

    оптимизация для мобильных устройств

--- src/components/HeroModels/Visual.jsx
+++ src/components/HeroModels/Visual.jsx
@@ -2,13 +2,15 @@ import { Canvas } from "@react-three/fiber";
 import Scene from "./Scene";
 
 const HeroVisual = () => {
+	const isMobile = typeof window !== "undefined" && window.innerWidth < 768;
+
 	return (
 		<Canvas
 			className="w-full h-screen"
 			camera={{ position: [0, 0, 7], fov: 45, near: 0.1, far: 100 }}
-			dpr={window.devicePixelRatio}
+			dpr={isMobile ? [1, 1.5] : [1, 2]}
 			gl={{
-				antialias: true,
+				antialias: !isMobile,
 				alpha: true,
 				powerPreference: "high-performance",
 			}}
